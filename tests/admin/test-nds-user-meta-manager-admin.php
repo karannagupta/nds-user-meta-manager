ee70bb94967c2acc03a88d806c22f5d6c7b28060
Karan Gupta <nuancedesignstudio@users.noreply.github.com>
2017-06-16 18:36:06 +0530

    Added PHPUnit test for the delete_user_meta functionality.

--- tests/admin/test-nds-user-meta-manager-admin.php
+++ tests/admin/test-nds-user-meta-manager-admin.php
@@ -30,18 +30,26 @@ class Nds_User_Meta_Manager_Admin_Test extends WP_UnitTestCase {
     }
     
     public function tearDown() {
+        //delete the mock
         unset($this->mock);
+        
         parent::tearDown();
     }
+           
         
     /*
      * Test that without the nonce and $REQUEST params wp_die is invoked
      */         
     public function test_nds_add_user_meta_form_response_with_incomplete_form() {  
         
-       fwrite(STDOUT, "\n\t\t Testing exceptions for invalid form parameters ...");
+       fwrite(STDOUT, "\n\t\t Testing exceptions for invalid (Add Meta) form parameters ..");
        
-       //$REQUEST does not have the required params at this point.
+       /*
+        * $REQUEST does not have the required params at this point.
+        * 
+        * This will lead to an exception with wp_die
+        */
+       
        $this->setExpectedException( 'WPDieException', 'Invalid nonce specified' );        
        $this->mock->nds_add_user_meta_form_response();
        
@@ -51,18 +59,18 @@ class Nds_User_Meta_Manager_Admin_Test extends WP_UnitTestCase {
      * Test nds_add_user_meta_form_response which is the callback after the add meta form has been submitted
      */
     public function test_nds_add_user_meta_form_response_with_complete_form() {
-        fwrite(STDOUT, "\n\t\t Testing callback for form to Add User Meta ...");
-        
-        $this->plugin_name = 'nds-user-meta-manager';
+        fwrite(STDOUT, "\n\t\t Testing callback for form to Add User Meta ..");                
         wp_set_current_user(1);
 
-        //Test with the nonce and other params set
+        /*
+         * Test the method with the required nonce and other params set in $_REQUEST
+         */
         $factory_user_id = $this->factory->user->create();
         $the_user_meta_before = get_user_meta($factory_user_id);
         
         //create the form post response params
         $_REQUEST['nds_add_user_meta_nonce'] = wp_create_nonce('nds_add_user_meta_form_nonce');  
-        $_REQUEST['nds']['user_meta_key'] = 'os';
+        $_REQUEST['nds']['user_meta_key'] = 'fav_os';
         $_REQUEST['nds']['user_meta_value'] = 'linux';
         $_REQUEST['nds_user'] = $factory_user_id;
                 
@@ -72,14 +80,72 @@ class Nds_User_Meta_Manager_Admin_Test extends WP_UnitTestCase {
                     ->will($this->returnValue(true));
         
         //call the method that adds the user_meta
-        $this->mock->nds_add_user_meta_form_response();
+        $this->mock->nds_add_user_meta_form_response();        
+        $the_user_meta_after = get_user_meta($factory_user_id);                
+        $this->assertGreaterThan( sizeof($the_user_meta_before), sizeof($the_user_meta_after) );                
         
-        $the_user_meta_after = get_user_meta($factory_user_id);
-        $this->assertGreaterThan( sizeof($the_user_meta_before), sizeof($the_user_meta_after) );
-        
-        $the_added_user_meta = get_user_meta($factory_user_id, 'os', true );
+        $the_added_user_meta = get_user_meta($factory_user_id, 'fav_os', true );
         $this->assertEquals('linux', $the_added_user_meta);        
             
     }
+    
+    /*
+     * Test that without the nonce and $REQUEST params wp_die is invoked
+     */         
+    public function test_nds_delete_user_meta_form_response_with_incomplete_form() {  
+        
+       fwrite(STDOUT, "\n\t\t Testing exceptions for invalid (Delete Meta) form parameters ..");
+       
+       /*
+        * $REQUEST does not have the required params at this point.
+        * 
+        * This will lead to an exception with wp_die
+        */
+       $this->setExpectedException( 'WPDieException', 'Invalid nonce specified' );        
+       $this->mock->nds_delete_user_meta_form_response();
+       
+    }       
+    
+    /*
+     * Test nds_delete_user_meta_form_response which is the callback after the delete meta form has been submitted
+     */
+    public function test_nds_delete_user_meta_form_response_with_complete_form() {
+        fwrite(STDOUT, "\n\t\t Testing callback for form to Delete User Meta ..");                
+        wp_set_current_user(1);
+
+        /*
+         * Test the method with the required nonce and other params set in $_REQUEST
+         */        
+        $factory_user_id = $this->factory->user->create();
+        $the_user_meta_before = get_user_meta($factory_user_id);
+        
+        //add some meta to the user to delete later
+        add_user_meta($factory_user_id, 'fav_cms', 'wordpress', false);
+                
+        $the_user_meta_after = get_user_meta($factory_user_id);
+        $the_added_user_meta = get_user_meta($factory_user_id, 'fav_cms', true );
+        
+        //verify the addition
+        $this->assertEquals('wordpress', $the_added_user_meta);
+        
+        //create the form post response params
+        $_REQUEST['nds_delete_user_meta_nonce'] = wp_create_nonce('nds_delete_user_meta_form_nonce');  
+        $_REQUEST['nds_delete_user_meta_key'] = 'fav_cms';        
+        $_REQUEST['nds_user'] = $factory_user_id;
+                
+        //nds_redirect redirects to the admin page. Let's believe it did
+        $this->mock->expects($this->any())
+                    ->method('nds_redirect')
+                    ->will($this->returnValue(true));
+                        
+        //call the method that deletes the user_meta       
+        $this->mock->nds_delete_user_meta_form_response();
+        $the_user_meta_after = get_user_meta($factory_user_id);
+        
+        $the_added_user_meta = get_user_meta($factory_user_id, 'fav_cms', true );
+        $this->assertEmpty($the_added_user_meta);
+        $this->assertEquals( sizeof($the_user_meta_before), sizeof($the_user_meta_after) );
+    }    
+    
     
 }
