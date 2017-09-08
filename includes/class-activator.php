0150f1833664db7212b2ac135567a85247ba6251
Karan Gupta <nuancedesignstudio@users.noreply.github.com>
2017-09-08 21:29:59 +0530

    Moved minimum PHP check to activator class

--- includes/class-activator.php
+++ includes/class-activator.php
@@ -23,6 +23,14 @@ class Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
+        
+		$min_php = '5.3.0';
+
+		// Check PHP Version and deactivate & die if it doesn't meet minimum requirements.
+		if ( version_compare( PHP_VERSION, $min_php, '<' ) ) {
+					deactivate_plugins( plugin_basename( __FILE__ ) );
+			wp_die( 'This plugin requires a minmum PHP Version of ' . $min_php );
+		}        
 
 	}
 
--- nds-user-meta-manager.php
+++ nds-user-meta-manager.php
@@ -26,9 +26,9 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-/*----------------------------------------------------------------------------*
+/**
  * Define Constants
- *----------------------------------------------------------------------------*/
+ */
 
 define( 'PLUGIN_NAME_DIR', plugin_dir_path( __FILE__ ) );
 
@@ -36,36 +36,36 @@ define( 'PLUGIN_NAME_URL', plugin_dir_url( __FILE__ ) );
 
 define( 'PLUGIN_NAME_BASENAME', plugin_basename( __FILE__ ) );
 
-/*----------------------------------------------------------------------------*
+/**
  * Autoload Classes
- *----------------------------------------------------------------------------*/
+ */
+
 require_once( PLUGIN_NAME_DIR . 'lib/autoload.php');
 
-/*----------------------------------------------------------------------------*
- * Register Activation and Deactivation Hooks
- *----------------------------------------------------------------------------*/
-
 /**
+ * Register Activation and Deactivation Hooks
+ *
  * The code that runs during plugin activation.
  * This action is documented in includes/class-activator.php
  */
+
 register_activation_hook( __FILE__, array('Nds_User_Meta_Manager\Includes\Activator', 'activate') );
 
 /**
  * The code that runs during plugin deactivation.
- * This action is documented in lib/class-nds-user-meta-manager-deactivator.php
+ * This action is documented in lib/class-deactivator.php
  */
+
 register_deactivation_hook( __FILE__, array('Nds_User_Meta_Manager\Includes\Deactivator', 'deactivate') );
 
 
-/*----------------------------------------------------------------------------*
- * Singleton Container
- *----------------------------------------------------------------------------*/
-/*
+/**
+ * Plugin Singleton Container
+ *
  * Maintains a single copy of the plugin app object
- * @since    1.1.0
+ *
+ * @since    1.0.0
  */
-
 class Nds_User_Meta_Manager {
 
         static $init;
@@ -87,33 +87,25 @@ class Nds_User_Meta_Manager {
     
 }
 
-/*----------------------------------------------------------------------------*
- * Begins execution of the plugin
- *----------------------------------------------------------------------------*/
 /*
+ * Begins execution of the plugin
  *
  * Since everything within the plugin is registered via hooks,
  * then kicking off the plugin from this point in the file does
  * not affect the page life cycle.
- * 
+ *
  * Also returns copy of the app object so 3rd party developers
  * can interact with the plugin's hooks contained within.
  *
  */
+
 function nds_user_meta_manager() {
         return Nds_User_Meta_Manager::init();
 }
 
 $min_php = '5.3.0';
 
-// Check the minimum required PHP version and run the plugin
+// Check the minimum required PHP version and run the plugin.
 if ( version_compare( PHP_VERSION, $min_php, '>=' ) ) {
-        nds_user_meta_manager();    
-}        
-else {
-        add_action( 'admin_notices', function() {
-                echo '<div class="error"><p><strong>Nds User Meta Manager</strong> cannot be activated due to incompatible PHP version.</p></div>';
-            }
-        );
+		nds_user_meta_manager();
 }
-
